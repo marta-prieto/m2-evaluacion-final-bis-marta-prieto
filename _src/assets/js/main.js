4ec18c27e1139a25ad2b4a0fc6b64f9910851a8f
Jorge <martaprietoluque@gmail.com>
2019-08-27 20:44:34 +0200

    fix github

---
+++ _src/assets/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+const input = document.querySelector('.input');
+const fourCards = document.querySelector('.four__cards');
+const sixCards = document.querySelector('.six__cards');
+const eightCards = document.querySelector('.eight__cards');
+const button = document.querySelector('.btn');
+const resultCards =document.querySelector('.result');
+const adalabCard = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
